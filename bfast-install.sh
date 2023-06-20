a5a789f50bf7ad06939a85008303dd7ae7b87a71
bubujoe <80191627+bubujoe@users.noreply.github.com>
2023-06-20 12:21:49 +0200

    Update bfast-install.sh

--- bfast-install.sh
+++ bfast-install.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+# B-Fast Printer Setup Files
+# Installer
+#
+# Copyright (C) 2023  bubujoe jozef.budai@gmail.com
+#
+KLIPPER_HOME="${HOME}/klipper"
+KLIPPER_CONFIG_HOME="${HOME}/printer_data/config"
+OLD_KLIPPER_CONFIG_HOME="${HOME}/klipper_config"
