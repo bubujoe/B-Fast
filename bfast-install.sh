c7ec382e9b58aaa036f905e94f1ce1ae165bfd77
bubujoe <80191627+bubujoe@users.noreply.github.com>
2023-06-20 12:26:45 +0200

    Update bfast-install.sh

--- bfast-install.sh
+++ bfast-install.sh
@@ -7,3 +7,50 @@
 KLIPPER_HOME="${HOME}/klipper"
 KLIPPER_CONFIG_HOME="${HOME}/printer_data/config"
 OLD_KLIPPER_CONFIG_HOME="${HOME}/klipper_config"
+
+function nextfilename {
+    local name="$1"
+    printf "%s-%s.%s" ${name%%.*} $(date '+%Y%m%d_%H%M%S') ${name#*.}
+}
+
+function nextsuffix {
+    local name="$1"
+    local -i num=0
+    while [ -e "$name.0$num" ]; do
+        num+=1
+    done
+    printf "%s.0%d" "$name" "$num"
+}
+
+verify_not_root() {
+    if [ "$EUID" -eq 0 ]; then
+        echo -e "${ERROR}This script must not run as root"
+        exit -1
+    fi
+}
+
+check_klipper() {
+    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
+        echo -e "${INFO}Klipper service found"
+    else
+        echo -e "${ERROR}Klipper service not found! Please install Klipper first"
+        exit -1
+    fi
+
+}
+
+verify_home_dirs() {
+    if [ ! -d "${KLIPPER_HOME}" ]; then
+        echo -e "${ERROR}Klipper home directory (${KLIPPER_HOME}) not found. Use '-k <dir>' option to override"
+        exit -1
+    fi
+    if [ ! -d "${KLIPPER_CONFIG_HOME}" ]; then
+        if [ ! -d "${OLD_KLIPPER_CONFIG_HOME}" ]; then
+            echo -e "${ERROR}Klipper config directory (${KLIPPER_CONFIG_HOME} or ${OLD_KLIPPER_CONFIG_HOME}) not found. Use '-c <dir>' option to override"
+            exit -1
+        fi
+        KLIPPER_CONFIG_HOME="${OLD_KLIPPER_CONFIG_HOME}"
+    fi
+    echo -e "${INFO}Klipper config directory (${KLIPPER_CONFIG_HOME}) found"
+}
+
